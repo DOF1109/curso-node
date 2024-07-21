2d4e42c6b3cf3355bf3535e53117c484134ea847
Daniel Omar Frias <friasdaniel.1109@gmail.com>
2024-07-21 15:44:11 -0300

    Se copia clase 4 y se instala mysql2

---
+++ 5 - MySQL/middlewares/cors.js
@@ -0,0 +1,27 @@
+import cors from "cors"; // para permitir CORS desde navegador
+
+// Metodos simples: GET, HEAD, POST
+// Metodos complejos: PUT, PATCH, DELETE
+
+// CORS PRE-Fligth --> para metodos complejos
+// Hace una peticion OPTIONS antes de hacer un PUT, PATCH o DELETE
+
+// Origenes acetados por CORS
+const ACCEPTED_ORIGINS = [
+  "http://localhost:8080",
+  "http://localhost:1234",
+  "https://movie.com",
+  "https://midu.dev",
+];
+
+const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
+  cors({
+    origin: (origin, callback) => {
+      if (acceptedOrigins.includes(origin) || !origin) {
+        return callback(null, true);
+      }
+      return callback(new Error("Not allowed by CORS"));
+    },
+  });
+
+export default corsMiddleware;
---
+++ 5 - MySQL/schemas/movies.js
@@ -0,0 +1,43 @@
+import zod from "zod" // para validaciones
+
+const movieSchema = zod.object({
+  title: zod.string({
+    invalid_type_error: "Title must be a string",
+    required_error: "Title is required",
+  }),
+  year: zod.number().int().min(1900).max(2024),
+  director: zod.string().min(3).max(50),
+  duration: zod.number().int().positive(),
+  poster: zod.string().url({
+    message: "Poter must be a valid url",
+  }),
+  genre: zod.array(
+    zod.enum([
+      "Action",
+      "Crime",
+      "Drama",
+      "Adventure",
+      "Sci-Fi",
+      "Romance",
+      "Animation",
+      "Biography",
+      "Fantasy",
+    ]),
+    {
+      required_error: "Genre is reuired",
+      invalid_type_error: "Genre must be an array of enum Genre",
+    }
+  ),
+  rate: zod.number().min(0).max(10).default(0),
+});
+
+export function validateMovie(object) {
+  // con safeParse() verifico más facil si tiene un error o datos
+  return movieSchema.safeParse(object);
+}
+
+export function validatePartialMovie(object) {
+  // con partial() solo valído las propiedades que se encuetran
+  // las demás las ignora
+  return movieSchema.partial().safeParse(object);
+}
