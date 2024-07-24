7fd74c3f83e2c6eb000ff1f7618bb0819412684c
Daniel Omar Frias <friasdaniel.1109@gmail.com>
2024-07-23 21:05:29 -0300

    "create" completo y falta terminar el "getById"

--- 5 - MySQL/models/mysql/movie.js
+++ 5 - MySQL/models/mysql/movie.js
@@ -38,12 +38,63 @@ export class MovieModel {
     const [movies] = await connection.query(
       "SELECT BIN_TO_UUID(id) AS id, title, year, director, duration, poster, rate FROM movie;"
     );
-    console.log(movies);
+    return movies;
   }
 
-  static async getById({ id }) {}
+  static async getById({ id }) {
+    const [movies] = await connection.query(
+      `SELECT BIN_TO_UUID(id) AS id, title, year, director, duration, poster, rate 
+        FROM movie
+        WHERE id = UUID_TO_BIN(?);`,
+      [id]
+    );
 
-  static async create({ input }) {}
+    if (movies.length === 0) return null;
+
+    return movies[0];
+  }
+
+  // falta extraer e insertar los generos
+  static async create({ input }) {
+    // Extraigo los valores del input
+    const {
+      genre: genreInput, // un array
+      title,
+      year,
+      duration,
+      director,
+      rate,
+      poster,
+    } = input;
+
+    // todo: crear la conexion de genre
+
+    // Genero un uuid con mysql
+    const [uuidResult] = await connection.query(`SELECT UUID() uuid;`);
+    const [{ uuid }] = uuidResult;
+
+    try {
+      // Inserto la pelicula en la BD
+      const result = await connection.query(
+        `INSERT INTO movie (id, title, year, duration, director, poster, rate)
+          VALUES (UUID_TO_BIN(?), ?, ?, ?, ?, ?, ?);`,
+        [uuid, title, year, duration, director, poster, rate]
+      );
+    } catch (error) {
+      throw new Error("Error creating movie");
+      // los detalles del error enviar a un servicio interno no al usuario
+      // sendLog(error)
+    }
+
+    const [movies] = await connection.query(
+      `SELECT BIN_TO_UUID(id) AS id, title, year, duration, director, rate, poster
+        FROM movie
+        WHERE id = UUID_TO_BIN(?);`,
+      [uuid]
+    );
+
+    return movies[0];
+  }
 
   static async delete({ id }) {}
 
