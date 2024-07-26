878d760a3bcaa90ff3db678f20d2aa5232109740
Daniel Omar Frias <friasdaniel.1109@gmail.com>
2024-07-25 21:12:53 -0300

    Servidor con web socket creado (backend)

--- 6 - Chat en tiempo real/server/index.js
+++ 6 - Chat en tiempo real/server/index.js
@@ -1,15 +1,26 @@
 import express from "express";
 import logger from "morgan";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
 const port = process.env.PORT ?? 3000;
 
+// Creamos un servidor http y que tenga la funcionalidad de socket.io
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+// Mostrar en consola los logs de las request al server
 app.use(logger("dev"));
+
+io.on("connetion", () => {
+  console.log("A user has connected!");
+});
 
 app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/client/index.html");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
